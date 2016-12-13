f555bfd49cbeb7ef72e354d747bf00d3da78f15d
kurusaki234 <0111338@kdu-online.com>
2016-12-13 08:01:17 +0800

    no message

--- Assets/Scripts/BuildingStat.cs
+++ Assets/Scripts/BuildingStat.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuildingStat : MonoBehaviour {
+public class BuildingStat : MonoBehaviour 
+{
+	bool empty;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Start()
+	{
+		empty = true;
 	}
 }
